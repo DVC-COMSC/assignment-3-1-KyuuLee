fbdc24ece9106719f8ee4f55fb7a6d137198fad8
Kyuu <69904775+KyuuLee@users.noreply.github.com>
2023-03-04 14:01:01 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    pass
-
+    import random
+    n1 = random.randint(0,100)
+    n2 = random.randint(0,100)
+    n3 = random.randint(0,100)
+    print (n1, n2, n3)
+    m = min(n1, n2, n3)
+    print (m)
 
 if __name__ == '__main__':
     main()
